aac6dadf477c8ad82b751a6490016626401462e8
Eduardo Peniche <148513763+eduardoPeniche@users.noreply.github.com>
2026-02-26 18:34:50 -0600

    fix: imports and presets for wire-button

--- resources/views/admin/roles/actions.blade.php
+++ resources/views/admin/roles/actions.blade.php
@@ -1,14 +1,14 @@
 <div class="flex items-center space-x-2">
-    <x-button class="bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 text-xs px-3 py-1.5" href="{{route('admin.roles.edit', $role)}}">
+    <x-wire-button href="{{route('admin.roles.edit', $role)}}" xs>
         <i class="fa-solid fa-pen-to-square"></i>
-    </x-button>
+    </x-wire-button>
 
     <form action="{{ route('admin.roles.destroy', $role) }}" method="POST" class="inline">
         @csrf
         @method('DELETE')
-        <x-button type="submit" class="bg-red-600 hover:bg-red-700 focus:ring-red-500 text-xs px-3 py-1.5">
+        <x-wire-button type="submit" red xs>
             <i class="fa-solid fa-trash"></i>
-        </x-button>
+        </x-wire-button>
     </form>
 
 </div>
